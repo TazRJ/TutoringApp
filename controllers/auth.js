49a2f2be5b19390b3d5ebb33855dbdb1c37ef1df
TazRJ <tarunraja2001@gmail.com>
2023-11-23 14:15:09 +1100

    styling

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,16 +2,16 @@ const passport = require('passport')
 const validator = require('validator')
 const User = require('../models/User')
 
- exports.getLogin = (req, res) => {
+module.exports = {
+  getLogin : (req, res) => {
     if (req.user) {
       return res.redirect('/home')
     }
     res.render('login', {
       title: 'Login'
     })
-  }
-  
-  exports.postLogin = (req, res, next) => {
+  },
+  postLogin : (req, res, next) => {
     const validationErrors = []
     if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
     if (validator.isEmpty(req.body.password)) validationErrors.push({ msg: 'Password cannot be blank.' })
@@ -34,9 +34,8 @@ const User = require('../models/User')
         res.redirect(req.session.returnTo || '/home')
       })
     })(req, res, next)
-  }
-  
-  exports.logout = (req, res) => {
+  },
+  logout : (req, res) => {
     req.logout(() => {
       console.log('User has logged out.')
     })
@@ -45,18 +44,18 @@ const User = require('../models/User')
       req.user = null
       res.redirect('/')
     })
-  }
+  },
   
-  exports.getSignup = (req, res) => {
+  getSignup : (req, res) => {
     if (req.user) {
       return res.redirect('/home')
     }
     res.render('signup', {
       title: 'Create Account'
     })
-  }
+  },
   
-  exports.postSignup = (req, res, next) => {
+  postSignup : (req, res, next) => {
     const validationErrors = []
     if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
     if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' })
@@ -94,3 +93,9 @@ const User = require('../models/User')
       })
     })
   }
+}
+ 
+  
+  
+  
+  
